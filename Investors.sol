525b1c80daf0ab8f3926c65dfcea97739a166126
jstafford <CatMouse2017!>
2022-04-02 10:21:06 -0500

    Added a todo comment with exercise goal to Investors.sol

--- Investors.sol
+++ Investors.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+// TODO:
+//      Build a smart contract given everything we've learnt up to this point that can add investor wallets
+//      to a decentralized bank and then allocate (pay) them funds.
 contract Investors {
 
 }
