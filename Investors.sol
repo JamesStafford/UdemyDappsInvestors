a3b40e243b48ab0f9ceea8b46f92d1af22829364
jstafford <CatMouse2017!>
2022-04-05 18:47:29 -0500

    Updated Investors.sol with details from pay-investors-assignment.sol along with premade checkInvestors method.

--- Investors.sol
+++ Investors.sol
@@ -5,18 +5,22 @@ pragma solidity ^0.8.0;
 //      Build a smart contract that can add investor wallets
 //      to a decentralized bank and then allocate (pay) them funds.
 //      Pay a few accounts some funds of your choose and when you're done run the checkInvestors testing function.
-contract Investors {
+contract AddressWallets {
     address bankAddress;
 
     constructor() payable {
         bankAddress = msg.sender;
     }
 
-    address payable[] investors;
-    mapping(address => uint) funds;
+    address payable[] investorWallets;
+    mapping(address => uint) investors;
 
-    function payAccount(address payable investorAddress, uint amount) public {
-        investors.push(investorAddress);
-        funds[investorAddress] = amount;
+    function payInvestors(address payable investorAddress, uint amount) public {
+        investorWallets.push(investorAddress);
+        investors[investorAddress] = amount;
+    }
+
+    function checkInvestors() public view returns (uint) {
+        return investorWallets.length;
     }
 }
