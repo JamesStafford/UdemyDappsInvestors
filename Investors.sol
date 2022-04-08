3d4ced509dd1408d7f305fad60d6f1bfe03f0b92
jstafford <CatMouse2017!>
2022-04-07 21:02:38 -0500

    Added goals of the second assignment

--- Investors.sol
+++ Investors.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+/*
+    Write in the functionality so that not only do our investors get added to the wallets with amounts; but those amounts equal real ether
+    payouts
+
+    Exercise:
+        1. Create a constructor function which can allocate an initial payable value to the contract upon deployment.
+        2. Create a function called payout which explicitly prohibits outside visibility in the strict sense.
+        3. Write a for loop in the function that iterates over all the wallets of the investors.
+        4. While iterating through the wallets the loop should return a transfers of ethers equal to the amounts in each wallet.
+        (hint: You need to transfer into the investorWallet by checking each investor address matched up to investorWallets of the index)
+        5. Write a function called makePayment which can then execute the payout function once deployed.
+        6. Deploy the contract and test for successful transactions. (Hint: watch out for wei conversations!!)
+*/
 contract AddressWallets {
     address payable[] investorWallets;
     mapping(address => uint) investors;
